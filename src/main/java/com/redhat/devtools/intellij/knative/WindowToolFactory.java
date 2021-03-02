38173213e46ac8f7ac4f48a52be40f776cd40056
Luca Stocchi <49404737+lstocchi@users.noreply.github.com>
2021-03-02 11:38:03 +0100

    add open in editor action for service (#3) (#10)
    * add open in editor action for service (#3)
    
    Signed-off-by: Luca Stocchi <lstocchi@redhat.com>
    
    * uncomment commented code
    
    Signed-off-by: Luca Stocchi <lstocchi@redhat.com>
    
    * add support for double click
    
    Signed-off-by: Luca Stocchi <lstocchi@redhat.com>
    
    * extend open in editor action to revision nodes
    
    Signed-off-by: Luca Stocchi <lstocchi@redhat.com>
    
    * clean
    
    Signed-off-by: Luca Stocchi <lstocchi@redhat.com>
    
    * remove unused imports
    
    Signed-off-by: Luca Stocchi <lstocchi@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/knative/WindowToolFactory.java
+++ src/main/java/com/redhat/devtools/intellij/knative/WindowToolFactory.java
@@ -13,20 +13,25 @@ package com.redhat.devtools.intellij.knative;
 import com.intellij.ide.util.treeView.AbstractTreeStructure;
 import com.intellij.ide.util.treeView.NodeRenderer;
 import com.intellij.openapi.Disposable;
+import com.intellij.openapi.actionSystem.ActionGroup;
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.ActionPlaces;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.PopupHandler;
 import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.content.ContentFactory;
 import com.intellij.ui.tree.AsyncTreeModel;
 import com.intellij.ui.tree.StructureTreeModel;
 import com.intellij.ui.treeStructure.Tree;
 import com.redhat.devtools.intellij.common.tree.MutableModelSynchronizer;
+import com.redhat.devtools.intellij.knative.listener.TreeDoubleClickListener;
+import com.redhat.devtools.intellij.knative.listener.TreePopupMenuListener;
 import com.redhat.devtools.intellij.knative.tree.KnTreeStructure;
-import org.jetbrains.annotations.NotNull;
-
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import org.jetbrains.annotations.NotNull;
 
 public class WindowToolFactory implements ToolWindowFactory {
     @Override
@@ -38,9 +43,13 @@ public class WindowToolFactory implements ToolWindowFactory {
             StructureTreeModel<KnTreeStructure> model = buildModel(structure, project);
             new MutableModelSynchronizer<>(model, structure, structure);
             Tree tree = new Tree(new AsyncTreeModel(model, project));
+            tree.putClientProperty(Constants.STRUCTURE_PROPERTY, structure);
             tree.setCellRenderer(new NodeRenderer());
-
+            ActionManager actionManager = ActionManager.getInstance();
+            ActionGroup group = (ActionGroup)actionManager.getAction("com.redhat.devtools.intellij.knative.tree");
+            PopupHandler.installPopupHandler(tree, group, ActionPlaces.UNKNOWN, actionManager, new TreePopupMenuListener());
             toolWindow.getContentManager().addContent(contentFactory.createContent(new JBScrollPane(tree), "", false));
+            new TreeDoubleClickListener(tree);
         } catch (IllegalAccessException | InvocationTargetException | InstantiationException | NoSuchMethodException e) {
             throw new RuntimeException((e));
         }
--- src/main/java/com/redhat/devtools/intellij/knative/kn/KnCli.java
+++ src/main/java/com/redhat/devtools/intellij/knative/kn/KnCli.java
@@ -91,6 +91,16 @@ public class KnCli implements Kn {
         return getCustomCollection(json, Revision.class);
     }
 
+    @Override
+    public String getServiceYAML(String name) throws IOException {
+        return ExecHelper.execute(command, envVars, "service", "describe", name, "-o", "yaml", "-n", getNamespace());
+    }
+
+    @Override
+    public String getRevisionYAML(String name) throws IOException {
+        return ExecHelper.execute(command, envVars, "revision", "describe", name, "-o", "yaml", "-n", getNamespace());
+    }
+
     private <T> List<T> getCustomCollection(String json, Class<T> customClass) throws IOException {
         if (!JSON_MAPPER.readTree(json).has("items")) return Collections.emptyList();
         if (JSON_MAPPER.readTree(json).get("items").isNull()) return Collections.emptyList();
