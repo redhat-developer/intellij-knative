ffe55e7e47cb2103946b54f664363346c0016e99
Luca Stocchi <lstocchi@redhat.com>
2022-01-07 09:21:07 +0100

    add test
    Signed-off-by: Luca Stocchi <lstocchi@redhat.com>

---
+++ src/test/java/com/redhat/devtools/intellij/knative/actions/UndeployActionTest.java
@@ -0,0 +1,37 @@
+package com.redhat.devtools.intellij.knative.actions;
+
+import com.redhat.devtools.intellij.knative.actions.func.UndeployAction;
+import com.redhat.devtools.intellij.knative.tree.ParentableNode;
+import com.redhat.devtools.intellij.knative.utils.TreeHelper;
+import org.junit.Test;
+import org.mockito.MockedStatic;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class UndeployActionTest extends ActionTest {
+
+    private static final String FUNCTION = "function";
+
+    @Test
+    public void ExecuteDelete_OneKnFunctionNodeSelected_DeleteOneFunction() throws IOException {
+        Map<String, Integer> typePerTimesCalled = new HashMap<>();
+        typePerTimesCalled.put(FUNCTION, 1);
+        executeUndeployAction(new ParentableNode[] {knFunctionNode}, typePerTimesCalled);
+    }
+
+    private void executeUndeployAction(ParentableNode[] fakeSelectedNodesToUndeployed, Map<String, Integer> typePerTimesCalled) throws IOException {
+        UndeployAction action = new UndeployAction();
+        try(MockedStatic<TreeHelper> treeHelperMockedStatic = mockStatic(TreeHelper.class)) {
+            treeHelperMockedStatic.when(() -> TreeHelper.getKnFunctionsTreeStructure(any())).thenReturn(null);
+            action.executeDelete(project, kn, fakeSelectedNodesToUndeployed);
+            verify(kn, times(typePerTimesCalled.getOrDefault(FUNCTION, 0))).deleteFunctions(any());
+        }
+    }
+}
