f9d30e9de0e45a01be72f98c6eb5deae6c0a1b7a
Luca Stocchi <lstocchi@redhat.com>
2022-02-10 17:22:12 +0100

    feat: show notification when undeploy action is complete (#98)
    Signed-off-by: Luca Stocchi <lstocchi@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/knative/actions/func/UndeployAction.java
+++ src/main/java/com/redhat/devtools/intellij/knative/actions/func/UndeployAction.java
@@ -1,18 +1,26 @@
 package com.redhat.devtools.intellij.knative.actions.func;
 
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
-import com.redhat.devtools.intellij.common.utils.UIHelper;
+import com.redhat.devtools.intellij.common.utils.ExecHelper;
 import com.redhat.devtools.intellij.knative.actions.DeleteAction;
 import com.redhat.devtools.intellij.knative.kn.Kn;
 import com.redhat.devtools.intellij.knative.tree.KnFunctionNode;
 import com.redhat.devtools.intellij.knative.tree.ParentableNode;
 import com.redhat.devtools.intellij.knative.utils.TreeHelper;
+import com.redhat.devtools.intellij.knative.utils.WatchHandler;
+import io.fabric8.kubernetes.client.Watcher;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
+
+import static com.redhat.devtools.intellij.knative.Constants.KIND_FUNCTION;
+import static com.redhat.devtools.intellij.knative.Constants.NOTIFICATION_ID;
 
 public class UndeployAction extends DeleteAction {
     public UndeployAction() {
@@ -32,10 +40,44 @@ public class UndeployAction extends DeleteAction {
     }
 
     private void deleteResources(Class type, Map<Class, List<ParentableNode>> resourcesByClass, Kn kncli) throws IOException {
-        List<String> resources = resourcesByClass.get(type).stream().map(x -> x.getName()).collect(Collectors.toList());
+        List<String> resources = resourcesByClass.get(type).stream().map(ParentableNode::getName).collect(Collectors.toList());
         if (type.equals(KnFunctionNode.class)) {
             kncli.deleteFunctions(resources);
+            notifyUndeploy(resources);
+            listenToUndeployComplete(kncli, resources);
         }
+    }
+
+    private void listenToUndeployComplete(Kn kn, List<String> resources) {
+        String id = "Undeploy" + System.currentTimeMillis();
+        AtomicInteger cont = new AtomicInteger();
+        WatchHandler.get(kn).watchResource(id, KIND_FUNCTION, (action, service) -> {
+            if (action == Watcher.Action.DELETED
+                    && resources.stream().anyMatch(res -> res.equalsIgnoreCase(service.getMetadata().getName()))) {
+                int resourcesUndeployed = cont.incrementAndGet();
+                if (resourcesUndeployed >= resources.size()) {
+                    notifyUndeployComplete(resources);
+                    WatchHandler.get(kn).remove(id);
+                }
+            }
+        });
+    }
+
+    private void notifyUndeployComplete(List<String> resources) {
+        String content = (resources.size() > 1 ? resources.size() + " functions" : "Function " + resources.get(0)) + " successfully undeployed.";
+        notifyUndeploy(content);
+    }
+
+    private void notifyUndeploy(List<String> resources) {
+        String content = "Undeploying " + (resources.size() > 1 ? resources.size() + " functions" : "function " + resources.get(0)) + " ...";
+        notifyUndeploy(content);
+    }
+
+    private void notifyUndeploy(String content) {
+        ExecHelper.submit(() -> {
+            Notification notification = new Notification(NOTIFICATION_ID, "Undeploy functions", content, NotificationType.INFORMATION);
+            Notifications.Bus.notify(notification);
+        });
     }
 
     @Override
