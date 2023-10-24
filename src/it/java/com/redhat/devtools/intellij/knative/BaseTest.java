4e5582a8ceb9183a136dea3feeec7c032ada0c1a
Stephane Bouchet <sbouchet@redhat.com>
2023-10-24 11:46:38 +0200

    chore: support IJ 2023.2
    fix test
    
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>

--- src/it/java/com/redhat/devtools/intellij/knative/BaseTest.java
+++ src/it/java/com/redhat/devtools/intellij/knative/BaseTest.java
@@ -11,35 +11,29 @@
 package com.redhat.devtools.intellij.knative;
 
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.ui.TestDialog;
 import com.intellij.testFramework.fixtures.BasePlatformTestCase;
-import com.intellij.testFramework.fixtures.CodeInsightTestFixture;
-import com.intellij.testFramework.fixtures.IdeaProjectTestFixture;
-import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory;
-import com.intellij.testFramework.fixtures.TestFixtureBuilder;
 import com.redhat.devtools.intellij.knative.kn.Kn;
 import com.redhat.devtools.intellij.knative.kn.KnCliFactory;
 import com.redhat.devtools.intellij.common.utils.MessagesHelper;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import org.apache.commons.io.IOUtils;
-import org.junit.After;
-import org.junit.Before;
 
 public class BaseTest extends BasePlatformTestCase {
 
-    private CodeInsightTestFixture myFixture;
     protected Project project;
     private TestDialog previousTestDialog;
-    protected Kn kn;
+    private Kn kn;
 
+    @Override
     public void setUp() throws Exception {
         super.setUp();
         previousTestDialog = MessagesHelper.setTestDialog(message -> 0);
         kn = KnCliFactory.getInstance().getKn(project).get();
     }
 
+    @Override
     public void tearDown() throws Exception {
         MessagesHelper.setTestDialog(previousTestDialog);
         super.tearDown();
@@ -47,5 +41,9 @@ public class BaseTest extends BasePlatformTestCase {
 
     protected String load(String name) throws IOException {
         return IOUtils.toString(BaseTest.class.getResource("/" + name), StandardCharsets.UTF_8);
+    }
+
+    protected Kn getKn(){
+        return this.kn;
     }
 }
--- src/it/java/com/redhat/devtools/intellij/knative/KnServiceTest.java
+++ src/it/java/com/redhat/devtools/intellij/knative/KnServiceTest.java
@@ -12,15 +12,10 @@ package com.redhat.devtools.intellij.knative;
 
 import com.redhat.devtools.intellij.knative.kn.Service;
 import com.redhat.devtools.intellij.knative.utils.KnHelper;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
-import org.junit.Test;
-
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 public class KnServiceTest extends BaseTest {
 
@@ -28,19 +23,19 @@ public class KnServiceTest extends BaseTest {
         final String serviceName = "test1";
         String serviceAsYAML = load("service.yaml").replace("foo", serviceName);
 
-        KnHelper.saveNew(kn, serviceAsYAML);
+        KnHelper.saveNew(getKn(), serviceAsYAML);
         // verify service has been created
-        List<Service> services = kn.getServicesList();
+        List<Service> services = getKn().getServicesList();
         assertTrue(services.stream().anyMatch(service -> service.getName().equals(serviceName)));
 
         // clean up and verify cleaning succeed
-        kn.deleteServices(services
+        getKn().deleteServices(services
                 .stream()
                 .filter(service -> service.getName().equals(serviceName))
                 .map(Service::getName)
                 .collect(Collectors.toList())
         );
-        services = kn.getServicesList();
+        services = getKn().getServicesList();
         assertFalse(services.stream().anyMatch(service -> service.getName().equals(serviceName)));
     }
 
@@ -49,22 +44,22 @@ public class KnServiceTest extends BaseTest {
         final String serviceName = "test2";
         String serviceAsYAML = load("service.yaml").replace("foo", serviceName);
 
-        KnHelper.saveNew(kn, serviceAsYAML);
+        KnHelper.saveNew(getKn(), serviceAsYAML);
         // verify service has been created
-        List<Service> services = kn.getServicesList();
+        List<Service> services = getKn().getServicesList();
         assertTrue(services.stream().anyMatch(service -> service.getName().equals(serviceName)));
 
-        Service service = kn.getService(serviceName);
+        Service service = getKn().getService(serviceName);
         assertEquals(service.getName(), serviceName);
 
         // clean up and verify cleaning succeed
-        kn.deleteServices(services
+        getKn().deleteServices(services
                 .stream()
                 .filter(svc -> svc.getName().equals(serviceName))
                 .map(Service::getName)
                 .collect(Collectors.toList())
         );
-        services = kn.getServicesList();
+        services = getKn().getServicesList();
         assertFalse(services.stream().anyMatch(svc -> svc.getName().equals(serviceName)));
     }
 }
